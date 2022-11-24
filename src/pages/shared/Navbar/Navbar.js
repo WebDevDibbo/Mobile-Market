1c4251a01c91fd42e376b59a41ddcd587b456a2b
dibbodas12 <dibbodas228@gmail.com>
2022-11-24 15:10:44 +0600

    footer section added

--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
       );
   return (
     // sticky top-0 z-10
-    <div className="navbar  bg-base-100 ">
+    <div className="navbar  bg-base-100 my-5">
     <div className="navbar-start">
       <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -42,7 +42,7 @@ const Navbar = () => {
           </svg>
         </label>
         <ul
-          tabIndex={1}
+          tabIndex={0}
           className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
         >
           {menuItems}
@@ -52,20 +52,12 @@ const Navbar = () => {
         Mobile Market
       </Link>
     </div>
-    <div className="navbar-end hidden lg:flex">
-      <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu menu-horizontal">{menuItems}</ul>
     </div>
-    <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-      <svg xmlns="http://www.w3.org/2000/svg"className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"
->
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h8m-8 6h16"
-        />
-      </svg>
-    </label>
+    <div className="navbar-end">
+    <Link to='/login'><button className="btn btn-outline btn-success">LogIn</button></Link>
+    </div>
   </div>
   );
 };
