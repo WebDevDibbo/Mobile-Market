462695ceec027b22f2a9bc773bc1c52472700fe9
dibbodas12 <dibbodas228@gmail.com>
2022-11-24 10:31:46 +0600

    navbar added

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,18 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+const {default:Home}  =require("../../pages/Home/Home/Home");
+
+
+ const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+export default router;
